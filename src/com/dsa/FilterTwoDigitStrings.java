2c3b66005a88cb31c16e7a22d874d83dfeea4847
Jeetendra Shrivastava <jeetendra.shrivastava@in.ey.com>
2025-11-10 15:15:11 +0530

    add new FilterTwoDigitStrings

---
+++ src/com/dsa/FilterTwoDigitStrings.java
@@ -0,0 +1,12 @@
+package com.dsa;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FilterTwoDigitStrings {
+	public static void main(String[] args) {
+		List<String> datas = List.of("100","20","a","3","b");
+		List<String> result = datas.stream().filter(s -> s.length()==2).collect(Collectors.toList());
+		System.out.println(result);
+	}
+}
