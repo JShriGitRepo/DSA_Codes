daffbc4fb5164971e31939e1e2b9500aea995938
Jeetendra Shrivastava <jeetendra.shrivastava@in.ey.com>
2025-11-11 11:11:39 +0530

    add new StringRotationTracker

--- src/com/dsa/FilterTwoDigitStrings.java
+++ src/com/dsa/FilterTwoDigitStrings.java
@@ -6,7 +6,7 @@ import java.util.stream.Collectors;
 public class FilterTwoDigitStrings {
 	public static void main(String[] args) {
 		List<String> datas = List.of("100","20","a","3","b");
-		List<String> result = datas.stream().filter(s -> s.length()==2).collect(Collectors.toList());
+		List<String> result = datas.stream().filter(s -> s.length()==3).collect(Collectors.toList());
 		System.out.println(result);
 	}
 }
