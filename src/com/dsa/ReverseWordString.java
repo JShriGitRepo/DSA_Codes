f677cc02961326859e6cbc2071af93616638434c
Jeetendra Shrivastava <jeetendra.shrivastava@in.ey.com>
2025-11-14 16:47:00 +0530

    fix

--- src/com/dsa/ReverseWordString.java
+++ src/com/dsa/ReverseWordString.java
@@ -2,7 +2,7 @@ package com.dsa;
 
 public class ReverseWordString {
 	public static void main(String[] args) {
-		String str = "madam";
+		String str = "Welcome";
 		String res = new StringBuilder(str).reverse().toString();
 		System.out.println(res);
 	}
