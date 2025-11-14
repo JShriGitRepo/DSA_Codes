a2f34784c1224da11a00d45a9b3f684eb2a641a2
Jeetendra Shrivastava <jeetendra.shrivastava@in.ey.com>
2025-11-14 14:03:15 +0530

    add in filterchar code

--- src/com/dsa/FilterChar.java
+++ src/com/dsa/FilterChar.java
@@ -4,10 +4,15 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class FilterChar {
-	public static void main(String [] args) {
-		List<String> datas = Arrays.asList("1","2","a","3","b"); // In java 8
-//		List<String> datas = List.of("1","2","a","3","b");  // since Java 9
-		List<String> result = datas.stream().filter(s -> s.matches("[a-z]")).collect(Collectors.toList());
+	public static void main(String[] args) {
+		List<String> datas = Arrays.asList("1", "2", "a", "3", "b","H","J"); // In java 8
+//		List<String> datas = List.of("1", "a", "2", "b", "H"); // since Java 9
+		List<String> result = datas.stream().filter(s -> s.matches("\\d+")).collect(Collectors.toList());
+		List<String> result1 = datas.stream().filter(s -> s.matches("[a-z]")).collect(Collectors.toList());
+		List<String> result2 = datas.stream().filter(s -> s.matches("[A-Z]")).collect(Collectors.toList());
 		System.out.println(result);
+		System.out.println(result1);
+		System.out.println(result2);
+
 	}
 }
